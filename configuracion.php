1281f0d92855ddb5fe60e5aaf7b6a19c4f95a9a4
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-11-27 09:11:34 -0400

    Config

---
+++ configuracion.php
@@ -0,0 +1,4 @@
+<?php
+$directory="metalmecanica/";		//Sub Directorio, Ej: "metalmecanica/" 
+							//si su sistema e encuentre directamente en el dominio sin ningun subdirectorio dejelo vacio Ej: ""
+?>
