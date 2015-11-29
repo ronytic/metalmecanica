4e1dbea1e04b86f9173567d0e1dafedcef4d98fc
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-11-29 00:38:14 -0400

    Cabecerahtml

--- cabecerahtml.php
+++ cabecerahtml.php
@@ -6,10 +6,10 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<title>Shamcey - Metro Style Admin Template</title>
+<title>METALDUGAR</title>
 
 <link rel="stylesheet" href="<?php echo $folder?>css/core/style.default.css" type="text/css" />
-
+<link rel="stylesheet" href="<?php echo $folder?>css/general.css" type="text/css" />
 <link rel="stylesheet" href="<?php echo $folder?>css/core/responsive-tables.css">
 <script type="text/javascript" src="<?php echo $folder?>js/core/jquery-1.9.1.min.js"></script>
 <script type="text/javascript" src="<?php echo $folder?>js/core/jquery-migrate-1.1.1.min.js"></script>
