0c1ec701106c740788ad9accf5bfea45f54c5b84
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-11-29 00:55:30 -0400

    Guardar

---
+++ proveedor/registro/guardar.php
@@ -0,0 +1,29 @@
+<?php
+include_once("../../login/check.php");
+extract($_POST);
+include_once("../../class/proveedor.php");
+$proveedor=new proveedor;
+$valores=array("nombreempresa"=>"'$nombreempresa'",
+                "direccion"=>"'$direccion'",
+                "email"=>"'$email'",
+                "telefono"=>"'$telefono'",
+                "referente"=>"'$referente'",
+                "materiaprima"=>"'$materiaprima'",
+                "descripcion"=>"'$descripcion'",
+
+            );
+$proveedor->insertarRegistro($valores);
+$folder="../../";
+include_once($folder."cabecerahtml.php");
+?>
+<?php include_once($folder."cabecera.php");?>
+<div class="widgetbox box-inverse">
+    <h4 class="widgettitle">Mensaje de Confirmación</h4>
+        <div class="widgetcontent wc1">
+            <h5>Sus Datos se Registraron Correctamente</h5>
+            <br>
+            <a href="./" class="btn btn-primary">NUEVO</a>
+            <a href="listar.php" class="btn btn-default">LISTAR</a>
+        </div>
+</div>
+<?php include_once($folder."pie.php");?>
