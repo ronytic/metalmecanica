725a6838b7ebd2127b03060f560fd17026f0a73b
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-11-28 22:50:46 -0400

    Cabecera

--- cabecera.php
+++ cabecera.php
@@ -6,97 +6,20 @@
     
     <div class="header">
         <div class="logo">
-            <a href="dashboard.php"><img src="images/metaldugar.jpg" alt="" /></a>
+            <a href="<?php echo $folder?>"><img src="<?php echo $folder?>imagenes/general/metaldugar.jpg" alt="" /></a>
         </div>
         <div class="headerinner">
             <!-- Inicio Modulo Usuario-->
             <ul class="headmenu">
-                <li class="odd">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                        <span class="count">4</span>
-                        <span class="head-icon head-message"></span>
-                        <span class="headmenu-label">Messages</span>
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li class="nav-header">Messages</li>
-                        <li><a href=""><span class="icon-envelope"></span> New message from <strong>Jack</strong> <small class="muted"> - 19 hours ago</small></a></li>
-                        <li><a href=""><span class="icon-envelope"></span> New message from <strong>Daniel</strong> <small class="muted"> - 2 days ago</small></a></li>
-                        <li><a href=""><span class="icon-envelope"></span> New message from <strong>Jane</strong> <small class="muted"> - 3 days ago</small></a></li>
-                        <li><a href=""><span class="icon-envelope"></span> New message from <strong>Tanya</strong> <small class="muted"> - 1 week ago</small></a></li>
-                        <li><a href=""><span class="icon-envelope"></span> New message from <strong>Lee</strong> <small class="muted"> - 1 week ago</small></a></li>
-                        <li class="viewmore"><a href="messages.html">View More Messages</a></li>
-                    </ul>
-                </li>
-                <li>
-                    <a class="dropdown-toggle" data-toggle="dropdown" data-target="#">
-                    <span class="count">10</span>
-                    <span class="head-icon head-users"></span>
-                    <span class="headmenu-label">New Users</span>
-                    </a>
-                    <ul class="dropdown-menu newusers">
-                        <li class="nav-header">New Users</li>
-                        <li>
-                            <a href="">
-                                <img src="images/photos/thumb1.png" alt="" class="userthumb" />
-                                <strong>Draniem Daamul</strong>
-                                <small>April 20, 2013</small>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="">
-                                <img src="images/photos/thumb2.png" alt="" class="userthumb" />
-                                <strong>Shamcey Sindilmaca</strong>
-                                <small>April 19, 2013</small>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="">
-                                <img src="images/photos/thumb3.png" alt="" class="userthumb" />
-                                <strong>Nusja Paul Nawancali</strong>
-                                <small>April 19, 2013</small>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="">
-                                <img src="images/photos/thumb4.png" alt="" class="userthumb" />
-                                <strong>Rose Cerona</strong>
-                                <small>April 18, 2013</small>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="">
-                                <img src="images/photos/thumb5.png" alt="" class="userthumb" />
-                                <strong>John Doe</strong>
-                                <small>April 16, 2013</small>
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="odd">
-                    <a class="dropdown-toggle" data-toggle="dropdown" data-target="#">
-                    <span class="count">1</span>
-                    <span class="head-icon head-bar"></span>
-                    <span class="headmenu-label">Statistics</span>
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li class="nav-header">Statistics</li>
-                        <li><a href=""><span class="icon-align-left"></span> New Reports from <strong>Products</strong> <small class="muted"> - 19 hours ago</small></a></li>
-                        <li><a href=""><span class="icon-align-left"></span> New Statistics from <strong>Users</strong> <small class="muted"> - 2 days ago</small></a></li>
-                        <li><a href=""><span class="icon-align-left"></span> New Statistics from <strong>Comments</strong> <small class="muted"> - 3 days ago</small></a></li>
-                        <li><a href=""><span class="icon-align-left"></span> Most Popular in <strong>Products</strong> <small class="muted"> - 1 week ago</small></a></li>
-                        <li><a href=""><span class="icon-align-left"></span> Most Viewed in <strong>Blog</strong> <small class="muted"> - 1 week ago</small></a></li>
-                        <li class="viewmore"><a href="charts.html">View More Statistics</a></li>
-                    </ul>
-                </li>
+                
                 <li class="right">
                     
                         <div class="userloggedinfo">
-                        <img src="images/photos/thumb1.png" alt="" />
+                        <!--<img src="images/photos/thumb1.png" alt="" />-->
                         <div class="userinfo">
                             <h5><? echo $_SESSION['nombre_me']?> </h5>
                             <ul>
-                                <li><a href="usuario_perfil.php">Editar Perfil</a></li>
-                                <li><a href="salir.php">Salir</a></li>
+                                <li><a href="<?php echo $folder?>login/logout.php">Salir</a></li>
                             </ul>
                         </div>
                     </div>
@@ -104,102 +27,7 @@
         
                 </li>
             </ul>
-           <!--Fin de MOdulo de Usuario-->
-            <ul class="headmenu">
-                <li class="odd">
-                    <a class="dropdown-toggle" data-toggle="dropdown" href="#">
-                        <span class="count">4</span>
-                        <span class="head-icon head-message"></span>
-                        <span class="headmenu-label">Messages</span>
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li class="nav-header">Messages</li>
-                        <li><a href=""><span class="icon-envelope"></span> New message from <strong>Jack</strong> <small class="muted"> - 19 hours ago</small></a></li>
-                        <li><a href=""><span class="icon-envelope"></span> New message from <strong>Daniel</strong> <small class="muted"> - 2 days ago</small></a></li>
-                        <li><a href=""><span class="icon-envelope"></span> New message from <strong>Jane</strong> <small class="muted"> - 3 days ago</small></a></li>
-                        <li><a href=""><span class="icon-envelope"></span> New message from <strong>Tanya</strong> <small class="muted"> - 1 week ago</small></a></li>
-                        <li><a href=""><span class="icon-envelope"></span> New message from <strong>Lee</strong> <small class="muted"> - 1 week ago</small></a></li>
-                        <li class="viewmore"><a href="messages.html">View More Messages</a></li>
-                    </ul>
-                </li>
-                <li>
-                    <a class="dropdown-toggle" data-toggle="dropdown" data-target="#">
-                    <span class="count">10</span>
-                    <span class="head-icon head-users"></span>
-                    <span class="headmenu-label">New Users</span>
-                    </a>
-                    <ul class="dropdown-menu newusers">
-                        <li class="nav-header">New Users</li>
-                        <li>
-                            <a href="">
-                                <img src="images/photos/thumb1.png" alt="" class="userthumb" />
-                                <strong>Draniem Daamul</strong>
-                                <small>April 20, 2013</small>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="">
-                                <img src="images/photos/thumb2.png" alt="" class="userthumb" />
-                                <strong>Shamcey Sindilmaca</strong>
-                                <small>April 19, 2013</small>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="">
-                                <img src="images/photos/thumb3.png" alt="" class="userthumb" />
-                                <strong>Nusja Paul Nawancali</strong>
-                                <small>April 19, 2013</small>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="">
-                                <img src="images/photos/thumb4.png" alt="" class="userthumb" />
-                                <strong>Rose Cerona</strong>
-                                <small>April 18, 2013</small>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="">
-                                <img src="images/photos/thumb5.png" alt="" class="userthumb" />
-                                <strong>John Doe</strong>
-                                <small>April 16, 2013</small>
-                            </a>
-                        </li>
-                    </ul>
-                </li>
-                <li class="odd">
-                    <a class="dropdown-toggle" data-toggle="dropdown" data-target="#">
-                    <span class="count">1</span>
-                    <span class="head-icon head-bar"></span>
-                    <span class="headmenu-label">Statistics</span>
-                    </a>
-                    <ul class="dropdown-menu">
-                        <li class="nav-header">Statistics</li>
-                        <li><a href=""><span class="icon-align-left"></span> New Reports from <strong>Products</strong> <small class="muted"> - 19 hours ago</small></a></li>
-                        <li><a href=""><span class="icon-align-left"></span> New Statistics from <strong>Users</strong> <small class="muted"> - 2 days ago</small></a></li>
-                        <li><a href=""><span class="icon-align-left"></span> New Statistics from <strong>Comments</strong> <small class="muted"> - 3 days ago</small></a></li>
-                        <li><a href=""><span class="icon-align-left"></span> Most Popular in <strong>Products</strong> <small class="muted"> - 1 week ago</small></a></li>
-                        <li><a href=""><span class="icon-align-left"></span> Most Viewed in <strong>Blog</strong> <small class="muted"> - 1 week ago</small></a></li>
-                        <li class="viewmore"><a href="charts.html">View More Statistics</a></li>
-                    </ul>
-                </li>
-                <li class="right">
-                    
-            <div class="userloggedinfo">
-                        <img src="images/photos/thumb1.png" alt="" />
-                        <div class="userinfo">
-                            <h5><? echo $_SESSION['nombre_me']?> </h5>
-                            <ul>
-                                <li><a href="usuario_perfil.php">Editar Perfil</a></li>
-                                <li><a href="salir.php">Salir</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    
-        
-                </li>
-            </ul>
-
+           
            <!-- Fin Modulo Usuario-->
             <!--headmenu-->
         </div>
@@ -209,13 +37,16 @@
         
         <div class="leftmenu">        
             <ul class="nav nav-tabs nav-stacked">
+            	
+                <li class="nav-header">MENU</li>
+                
                 <li class="dropdown"><a href=""><span class="iconfa-pencil"></span> Forms</a>
-                        <ul>
-                            <li><a href="forms.html">Form Styles</a></li>
-                            <li><a href="wizards.html">Wizard Form</a></li>
-                            <li><a href="wysiwyg.html">WYSIWYG</a></li>
-                        </ul>
-                    </li>
+                	<ul>
+                    	<li><a href="forms.html">Form Styles</a></li>
+                        <li><a href="wizards.html">Wizard Form</a></li>
+                        <li><a href="wysiwyg.html">WYSIWYG</a></li>
+                    </ul>
+                </li>
             </ul>
         </div><!--leftmenu-->
         
@@ -224,19 +55,9 @@
     <div class="rightpanel">
         
         <ul class="breadcrumbs">
-            <li><a href="dashboard.html"><i class="iconfa-home"></i></a> <span class="separator"></span></li>
+            <li><a href="<?php echo $folder?>index.php"><i class="iconfa-home"></i></a> <span class="separator"></span></li>
             <li>INICIO</li>
-            <li class="right">
-                    
-                    <ul class="dropdown-menu pull-right skin-color">
-                        <li><a href="default">Default</a></li>
-                        <li><a href="navyblue">Navy Blue</a></li>
-                        <li><a href="palegreen">Pale Green</a></li>
-                        <li><a href="red">Red</a></li>
-                        <li><a href="green">Green</a></li>
-                        <li><a href="brown">Brown</a></li>
-                    </ul>
-            </li>
+            
         </ul>
         
         <div class="pageheader">
--- cabecerahtml.php
+++ cabecerahtml.php
@@ -1,5 +1,5 @@
-<?
-session_start();
+<?php
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -7,18 +7,21 @@ session_start();
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 <title>Shamcey - Metro Style Admin Template</title>
-<link rel="stylesheet" href="css/style.default.css" type="text/css" />
 
-<link rel="stylesheet" href="css/responsive-tables.css">
-<script type="text/javascript" src="js/jquery-1.9.1.min.js"></script>
-<script type="text/javascript" src="js/jquery-migrate-1.1.1.min.js"></script>
-<script type="text/javascript" src="js/jquery-ui-1.9.2.min.js"></script>
-<script type="text/javascript" src="js/modernizr.min.js"></script>
-<script type="text/javascript" src="js/bootstrap.min.js"></script>
-<script type="text/javascript" src="js/jquery.cookie.js"></script>
+<link rel="stylesheet" href="<?php echo $folder?>css/core/style.default.css" type="text/css" />
+
+<link rel="stylesheet" href="<?php echo $folder?>css/core/responsive-tables.css">
+<script type="text/javascript" src="<?php echo $folder?>js/core/jquery-1.9.1.min.js"></script>
+<script type="text/javascript" src="<?php echo $folder?>js/core/jquery-migrate-1.1.1.min.js"></script>
+<script type="text/javascript" src="<?php echo $folder?>js/core/jquery-ui-1.9.2.min.js"></script>
+<script type="text/javascript" src="<?php echo $folder?>js/core/modernizr.min.js"></script>
+<script type="text/javascript" src="<?php echo $folder?>js/core/bootstrap.min.js"></script>
+
+<script type="text/javascript" src="<?php echo $folder?>js/core/jquery.cookie.js"></script>
+<!--
 <script type="text/javascript" src="js/jquery.uniform.min.js"></script>
 <script type="text/javascript" src="js/flot/jquery.flot.min.js"></script>
 <script type="text/javascript" src="js/flot/jquery.flot.resize.min.js"></script>
-<script type="text/javascript" src="js/responsive-tables.js"></script>
-<script type="text/javascript" src="js/custom.js"></script>
+<script type="text/javascript" src="js/responsive-tables.js"></script>-->
+<script type="text/javascript" src="<?php echo $folder?>js/core/custom.js"></script>
 <!--[if lte IE 8]><script language="javascript" type="text/javascript" src="js/excanvas.min.js"></script><![endif]-->
