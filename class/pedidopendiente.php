10b4a46d28664492e6bb59da5e575b97ef7dd82e
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-12-06 22:56:47 -0400

    Clase Pedido Pendiente

---
+++ class/pedidopendiente.php
@@ -0,0 +1,6 @@
+<?php
+include_once("bd.php");
+class pedidopendiente extends bd{
+	var $tabla="pedidopendiente";
+}
+?>
---
+++ class/pedidopendientedetalle.php
@@ -0,0 +1,6 @@
+<?php
+include_once("bd.php");
+class pedidopendientedetalle extends bd{
+	var $tabla="pedidopendientedetalle";
+}
+?>
