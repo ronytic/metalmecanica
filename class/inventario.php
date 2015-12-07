979c2fdddeeb7f33632e2cf401ee0c4f0e50e1ab
Ronald Franz Nina Layme <ronald.nina.layme@gmail.com>
2015-12-06 21:45:28 -0400

    Class Inventario

--- class/inventario.php
+++ class/inventario.php
@@ -2,5 +2,9 @@
 include_once("bd.php");
 class inventario extends bd{
 	var $tabla="inventario";
+    function SumaTotal($codmateriaprima){
+        $this->campos=array("sum(cantidadstock) as cantidadstocktotal");
+        return $this->getRecords("codmateriaprima=$codmateriaprima and activo=1");
+    }
 }
 ?>
